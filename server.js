400530b6edeb444081a8080b52e55d7b2d0213d4
Adnan Mujagić <adnan.mujagic@edu.fit.ba>
2018-12-12 11:00:49 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        title: "Our site is under maintance!",
+        subtitle: "Please visit us back soon."
+    });
+});
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear()+1;
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs',{
+        pageTitle:'About page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000);
