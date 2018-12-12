2e8b7c7e30a0325d3471e3df0df204562434c7ff
Adnan Mujagić <adnan.mujagic@edu.fit.ba>
2018-12-12 15:17:36 +0100

    Added home route

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        title: "Our site is under maintance!",
-        subtitle: "Please visit us back soon."
-    });
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         title: "Our site is under maintance!",
+//         subtitle: "Please visit us back soon."
+//     });
+// });
 app.use(express.static(__dirname + '/public'));
+app.get('/', (req,res) => {
+    res.render('Home.hbs');
+});
+
 
 hbs.registerHelper('getCurrentYear', () =>{
     return new Date().getFullYear()+1;
+});
+
+
+
+app.get('/project',(req,res) => {
+    res.render('project.hbs', {
+        projectName: "My first Node.js project!",
+        projectDate: new Date().getDate()
+    });
 });
 
 app.get('/about', (req,res) => {
